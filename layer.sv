6dd789df4fb5fac036cdbef76b82c8e46c6a927b
Do Viet Dung <dovietdungsl@gmail.com>
2025-11-02 09:13:12 +0700

    [ADD] add layer.sv as model for layer and modify parameter name as explained in readme

---
+++ layer.sv
@@ -0,0 +1,36 @@
+module layer #(
+    parameter LAYER_DATA_WIDTH,
+    parameter NEURON_NUM,
+    parameter B_BITS,
+    parameter LAYER_NUM
+)(
+    input clk,
+    input rst_n,
+
+    input reg signed [B_BITS-1 : 0] bias [0: LAYER_NUM-1],
+    input reg signed [LAYER_DATA_WIDTH-1:0]data_in [0: NEURON_NUM-1],
+    input reg signed [31:0] weights [0: LAYER_NUM-1][0: NEURON_NUM-1],
+    input activation_func,
+
+    output signed [LAYER_DATA_WIDTH+7 : 0] data_out [0: LAYER_NUM - 1]
+);
+
+    genvar i;
+    generate
+        for (i = 0; i < LAYER_NUM; i = i + 1) begin : gen_neuron
+            neuron #(
+                .LAYER_DATA_WIDTH(LAYER_DATA_WIDTH),
+                .NEURON_NUM(NEURON_NUM),
+                .B_BITS(B_BITS)
+            ) u_neuron (
+                .clk(clk),
+                .rst_n(rst_n),
+                .activation_func(activation_func),
+                .weights(weights[i]),
+                .data_in(data_in),
+                .bias(bias[i]),
+                .neuron_out(data_out[i])
+            );
+        end
+    endgenerate
+endmodule
--- neuron.sv
+++ neuron.sv
@@ -1,25 +1,23 @@
 module neuron #(
-    parameter DATA_WIDTH ,
-    parameter NEURON_WIDTH,
-    parameter NEURON_BITS,
-    parameter COUNTER_END,
+    parameter LAYER_DATA_WIDTH,
+    parameter NEURON_NUM,
     parameter B_BITS
 ) (
     input clk,
     input rst_n,
     input activation_func,
-    input reg signed [DATA_WIDTH-1 : 0] weights [0:NEURON_WIDTH - 1],
-    input reg signed [NEURON_BITS-1 : 0] data_in [0:NEURON_WIDTH - 1],
+    input reg signed [31 : 0] weights [0:NEURON_NUM - 1],
+    input reg signed [LAYER_DATA_WIDTH-1 : 0] data_in [0:NEURON_NUM - 1],
     input reg signed [B_BITS-1 : 0] bias,
     
-    output reg signed [NEURON_BITS + 7 : 0] neuron_out
+    output reg signed [LAYER_DATA_WIDTH + 7 : 0] neuron_out
 ); 
 
-wire signed [DATA_WIDTH-1 : 0] bus_w;
-wire signed [NEURON_BITS-1 : 0] bus_x;
+wire signed [31 : 0] bus_w;
+wire signed [LAYER_DATA_WIDTH-1 : 0] bus_x;
 
-reg signed [DATA_WIDTH + NEURON_BITS -1 : 0] mult_result;
-reg signed [DATA_WIDTH + NEURON_BITS + 7 : 0] adder_result;
+reg signed [LAYER_DATA_WIDTH + 32 -1 : 0] mult_result;
+reg signed [LAYER_DATA_WIDTH + 32 + 7 : 0] adder_result;
 always @(posedge clk or negedge rst_n) begin
     if (!rst_n) begin
         neuron_out <= 0;
@@ -27,7 +25,7 @@ always @(posedge clk or negedge rst_n) begin
     else begin
         integer i;
         adder_result = 0;
-        for (i = 0; i < NEURON_WIDTH; i = i + 1) begin
+        for (i = 0; i < NEURON_NUM; i = i + 1) begin
             bus_w = weights[i];
             bus_x = data_in[i];
             mult_result = bus_w * bus_x;
@@ -35,15 +33,15 @@ always @(posedge clk or negedge rst_n) begin
         end
         // Apply activation function (ReLU)
         if (activation_func) begin
-            if (adder_result > 0) begin
-                neuron_out <= adder_result[NEURON_BITS + 7 : 0];
+            if (!adder_result[LAYER_DATA_WIDTH + 7]) begin
+                neuron_out <= adder_result[LAYER_DATA_WIDTH + 7 : 0];
             end
             else begin
                 neuron_out <= 0;
             end
         end
         else begin
-            neuron_out <= adder_result[NEURON_BITS + 7 : 0];
+            neuron_out <= adder_result[LAYER_DATA_WIDTH + 7 : 0];
         end
     end
 end
